c078507be489d98db105e1a2edd7053e36f86201
Ian Walter <public@iankwalter.com>
2019-07-25 21:59:01 -0400

    Fixing test regex

--- tests.js
+++ tests.js
@@ -10,7 +10,7 @@ test('timer', ({ expect }) => {
       expect(time[0]).toBe(2)
       expect(time[1]).toBeDefined()
       print.debug(duration)
-      expect(duration).toMatch(/2s (4|5[0-9]{2})\.([0-9]{3})ms/)
+      expect(duration).toMatch(/2s (4|5[0-9]{2})\.([0-9]+)ms/)
       resolve()
     }, 2500)
   })
